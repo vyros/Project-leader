75ae4d8f3b740164b9b38e676a5d802a507de400
vyros <j.deweer@gmail.com>
2012-02-20 11:40:37 +0100

    vyros

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html><body><h1><?php echo 'It works!'; ?></h1>
 <p><?php phpinfo(); ?></p>
 <p><?php echo 'Git c\'est bien mais un peu prise de tête un peu !'; ?></p>
+<p><?php echo 'Git c\'est bien mais un peu prise de tête un beaucoup !'; ?></p>
 <p>This is the default web page for this server.</p>
 <p>The web server software is running but no content has been added, yet.</p>
 </body></html>
