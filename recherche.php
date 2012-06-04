b8f741d399e9941f18db36c5a0ebcb96c13a3719
Alex <Alex@172.16.100.115>
2012-06-04 15:52:26 +0200

    commit pour modif recherche
    km3l3on

--- recherche.php
+++ recherche.php
@@ -4,59 +4,71 @@ header("Content-Type: text/plain");
 include_once("models/classSite.php");
 Site::init();
 ?>
-<div>
-    <div style="color:red">
-        Développement Web / Software
-    </div>
+<form>
     <div>
-        <?php
-        $array = Categorie::getListCategoriesFilsByCode(1);
-        foreach ($array as &$value) { ?>
-            <input type="checkbox" name=<?php $value[cat_libelle] ?> value= <?php $value[cat_libelle] ?> >
-            </br> <?php
-        }
-        ?>
+        <div style="color:red">
+            Développement Web / Software
+        </div>
+        <div style="height:250px">
+            <?php
+            $array = Categorie::getListCategoriesFilsByCode(1); 
+            $i = 0; 
+            /* Sur 4 colonnes */
+            foreach ($array as &$value) {  
+               if ($i == 4)
+               {
+                   ?></br><?php
+                   $i = 0;
+               }
+               $i++; ?>                  
+               <input style="" type="checkbox" name=<?php echo $value[cat_libelle] ?> value=<?php echo $value[cat_libelle] ?> /><?php echo $value[cat_libelle] ?>
+               <?php }               
+            ?>
+        </div>
     </div>
-</div>
 
-<div>
-    <div style="color:red">
-        Mobile
-    </div>
     <div>
-         <?php
-        $array = Categorie::getListCategoriesFilsByCode(2);
-        foreach ($array as &$value) {
-            echo $value[cat_libelle]; ?> </br> <?php
-        }
-        ?>
+        <div style="color:red">
+            Mobile
+        </div>
+       <div style="height:250px">
+            <?php
+            $array = Categorie::getListCategoriesFilsByCode(2);
+             foreach ($array as &$value) { ?>
+                <input type="checkbox" name=<?php echo $value[cat_libelle] ?> value=<?php echo $value[cat_libelle] ?> /><?php echo $value[cat_libelle] ?><br />
+                </br> <?php
+            }
+            ?>
+        </div>
     </div>
-</div>
 
-<div>
-    <div style="color:red">
-        Base de données
-    </div>
     <div>
-         <?php
-        $array = Categorie::getListCategoriesFilsByCode(3);
-        foreach ($array as &$value) {
-            echo $value[cat_libelle]; ?> </br> <?php
-        }
-        ?>
+        <div style="color:red">
+            Base de données
+        </div>
+        <div style="height:250px">
+            <?php
+            $array = Categorie::getListCategoriesFilsByCode(3);
+             foreach ($array as &$value) { ?>
+                <input type="checkbox" name=<?php echo $value[cat_libelle] ?> value=<?php echo $value[cat_libelle] ?> /><?php echo $value[cat_libelle] ?><br />
+                </br> <?php
+            }
+            ?>
+        </div>
     </div>
-</div>
 
-<div>
-    <div style="color:red">
-        Design
-    </div>
     <div>
-         <?php
-        $array = Categorie::getListCategoriesFilsByCode(4);
-        foreach ($array as &$value) {
-            echo $value[cat_libelle]; ?> </br> <?php
-        }
-        ?>
+        <div style="color:red">
+            Design
+        </div>
+        <div style="height:250px">
+            <?php
+            $array = Categorie::getListCategoriesFilsByCode(4);
+             foreach ($array as &$value) { ?>
+                <input type="checkbox" name=<?php echo $value[cat_libelle] ?> value=<?php echo $value[cat_libelle] ?> /><?php echo $value[cat_libelle] ?><br />
+                </br> <?php
+            }
+            ?>
+        </div>
     </div>
-</div>
+</form>
