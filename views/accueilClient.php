7fa22d6c65be59dfda1f31124ec9039ab496e4de
vyros <j.deweer@gmail.com>
2012-06-07 10:24:58 +0200

    Commit pour retouches css
    vyros

--- views/accueilClient.php
+++ views/accueilClient.php
@@ -14,69 +14,77 @@
     });
 </script>-->
 <div class="content_col_w420 fl">
-    <img class="imgIconProjet" src="images/icone_projet.png"/> 
-    <div class="header_02"> Vos derniers projets</div>
-    <div class="testimonial_box_wrapper">
-        <div class="testimonial_box">
-            <p>
-                <?php
-                $i = 0;
-                if (!is_null($lstUtilisateurProjetObjs)) {
-                    foreach ($lstUtilisateurProjetObjs as $objProjet) {
-                        ?>
-                                    <!--a class="jsLinkGetControllerView arg-projet arg-liste arg-<?php //echo $objProjet->getId();   ?>"-->
-                        <a onclick="getView('projet', 'liste', '<?php echo $objProjet->getId(); ?>');">
-                            <?php echo $objProjet->getLibelle(); ?></a><br />
-                        <?php
-                        $i++;
+    <div class="sub_content_col">
+        <img class="imgIconProjet" src="images/icone_projet.png"/> 
+        <div class="header_02"> Vos derniers projets</div>
+        <div class="testimonial_box_wrapper">
+            <div class="testimonial_box">
+                <p>
+                    <?php
+                    $i = 0;
+                    if (!is_null($lstUtilisateurProjetObjs)) {
+                        foreach ($lstUtilisateurProjetObjs as $objProjet) {
+                            ?>
+                                                <!--a class="jsLinkGetControllerView arg-projet arg-liste arg-<?php //echo $objProjet->getId();    ?>"-->
+                            <a onclick="getView('projet', 'liste', '<?php echo $objProjet->getId(); ?>');">
+                                <?php echo $objProjet->getLibelle(); ?></a><br />
+                            <?php
+                            $i++;
+                        }
                     }
-                }
-                ?>
-            </p>
-        </div>
-    </div>
-    <?php
-    if ($i != 0) {
-        ?>
-        <div class="section_w140 fr">
-            <div class="rc_btn_02"><a onclick="getView('projet', 'ajouter');">Créer un projet</a></div>
-            <div class="cleaner"></div>            
+                    ?>
+                </p>
+            </div>
         </div>
         <?php
-    } else {
-        echo "Aucun projet en cours";
+        if ($i != 0) {
+            ?>
+            <div class="section_w140 fr">
+                <div class="rc_btn_02"><a onclick="getView('projet', 'ajouter');">Créer un projet</a></div>
+                <div class="cleaner"></div>            
+            </div>
+            <?php
+        } else {
+            echo "Aucun projet en cours";
+            ?>
+            <div class="section_w140 fr">
+                <div class="rc_btn_02"><a onclick="getView('projet', 'ajouter');">Créer votre projet</a></div>
+                <div class="cleaner"></div>            
+            </div>
+            <?php
+        }
         ?>
-        <div class="section_w140 fr">
-            <div class="rc_btn_02"><a onclick="getView('projet', 'ajouter');">Créer votre projet</a></div>
-            <div class="cleaner"></div>            
-        </div>
-        <?php
-    }
-    ?>
 
-    <div class="margin_bottom_10 border_bottom"></div>
-    <div class="margin_bottom_30"></div>
-    <img class="imgIconPresta" src="images/icone_presta.png"/> 
-    <div class="header_02">Liste de prestataires</div>
-    <div class="testimonial_box_wrapper">
-        <div class="testimonial_box">
-            <p>
-                <?php
-                if (!is_null($lstUtilisateurObjs)) {
-                    foreach ($lstUtilisateurObjs as $objUtilisateur) {
-                        ?>
-                        <a onclick="getView('utilisateur', 'profil', '<?php echo $objUtilisateur->getId(); ?>');">
-                            <?php echo $objUtilisateur; ?></a><br />
-                        <?php
+        <div class="margin_bottom_10 border_bottom"></div>
+        <div class="margin_bottom_30"></div>
+
+    </div><!-- end of a section -->
+
+    <div class="sub_content_col">
+        <img class="imgIconPresta" src="images/icone_presta.png"/> 
+        <div class="header_02">Liste de prestataires</div>
+        <div class="testimonial_box_wrapper">
+            <div class="testimonial_box">
+                <p>
+                    <?php
+                    if (!is_null($lstUtilisateurObjs)) {
+                        foreach ($lstUtilisateurObjs as $objUtilisateur) {
+                            ?>
+                            <a onclick="getView('utilisateur', 'profil', '<?php echo $objUtilisateur->getId(); ?>');">
+                                <?php echo $objUtilisateur; ?></a><br />
+                            <?php
+                        }
                     }
-                }
-                ?>
-            </p>
+                    ?>
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="margin_bottom_20"></div>
-</div><!-- end of a section -->
 
+        <div class="margin_bottom_20 border_bottom"></div>
+        <div class="margin_bottom_30"></div>
+
+    </div><!-- end of a section -->
+</div>
 
 <div class="content_col_w420 fr">
     <img class="imgIconCompte" src="images/icone_compte.png"/> 
@@ -90,5 +98,6 @@
 
     <div class="margin_bottom_20 border_bottom"></div>
     <div class="margin_bottom_30"></div>
+
 </div>
 <img class="imgAcc" src="images/demilogo2.png"/>
